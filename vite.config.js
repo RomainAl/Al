751189010373a9d23092f71f476afb0c4a9a8acf
Romain Al <romainal@gmail.com>
2024-06-07 01:49:12 +0200

    try base vite

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import restart from 'vite-plugin-restart'
 export default {
     root: 'src/',
     publicDir: '../public/',
+    base: '/Al/',
     plugins:
     [
         // Restart server on static/public file change
